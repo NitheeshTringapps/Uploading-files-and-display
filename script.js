bbf34d9cda1873f0f6b56a964475cf5ad2a63071
NitheeshTringapps <96728775+NitheeshTringapps@users.noreply.github.com>
2022-03-14 11:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-// function display(event){
-//     // event.preventDefault();
-//     var image = document.querySelector("#image");
-//     var uploadedImage = document.querySelector("#uploadedImage");
+function saveLocalStorage(){
+    var image = document.querySelector("#image");
+    var json = document.querySelector("#json");
 
-//     var json = document.querySelector("#json");
-//     var jsonOutput = document.querySelector("#jsonOutput");
-
-//     var imageReader = new FileReader();
-//     imageReader.readAsDataURL(image.files[0])
-//     imageReader.onload=function(){
-//         uploadedImage.src = imageReader.result;
-//         // console.log(reader.result);
-//     }
+    var imageReader = new FileReader();
+    imageReader.readAsDataURL(image.files[0])
+    imageReader.onload=function(){
+        localStorage.setItem("IMAGE", imageReader.result);
+    }
     
-//     var jsonText = "";
-//     var reader = new FileReader();
-//     reader.readAsText(json.files[0]);
-//     reader.onload=function(){
-//         var jsonArray = JSON.parse(reader.result);
-//         // console.log(JSON.parse(reader.result));
-//         // console.log(jsonArray["name"]);
-//         for (var key in jsonArray){
-//             jsonText = jsonText + key + ": " + jsonArray[key] + "<br>";
-//         }
-//         jsonOutput.innerHTML = jsonText;
-//     }
-// }
+    var reader = new FileReader();
+    reader.readAsText(json.files[0]);
+    reader.onload=function(){
+        localStorage.setItem("JSON", reader.result);
+    }
+}
