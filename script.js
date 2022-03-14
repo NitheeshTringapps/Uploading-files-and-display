d4935b2324a7a3451ea6d6763fe2023e0675841d
NitheeshTringapps <96728775+NitheeshTringapps@users.noreply.github.com>
2022-03-14 09:19:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function display(event){
-    event.preventDefault();
-    var image = document.querySelector("#image");
-    var uploadedImage = document.querySelector("#uploadedImage");
+// function display(event){
+//     // event.preventDefault();
+//     var image = document.querySelector("#image");
+//     var uploadedImage = document.querySelector("#uploadedImage");
 
-    var json = document.querySelector("#json");
-    var jsonOutput = document.querySelector("#jsonOutput");
+//     var json = document.querySelector("#json");
+//     var jsonOutput = document.querySelector("#jsonOutput");
 
-    var imageReader = new FileReader();
-    imageReader.readAsDataURL(image.files[0])
-    imageReader.onload=function(){
-        uploadedImage.src = imageReader.result;
-        // console.log(reader.result);
-    }
+//     var imageReader = new FileReader();
+//     imageReader.readAsDataURL(image.files[0])
+//     imageReader.onload=function(){
+//         uploadedImage.src = imageReader.result;
+//         // console.log(reader.result);
+//     }
     
-    var jsonText = "";
-    var reader = new FileReader();
-    reader.readAsText(json.files[0]);
-    reader.onload=function(){
-        var jsonArray = JSON.parse(reader.result);
-        // console.log(JSON.parse(reader.result));
-        // console.log(jsonArray["name"]);
-        for (var key in jsonArray){
-            jsonText = jsonText + key + ": " + jsonArray[key] + "<br>";
-        }
-        jsonOutput.innerHTML = jsonText;
-    }
-}
+//     var jsonText = "";
+//     var reader = new FileReader();
+//     reader.readAsText(json.files[0]);
+//     reader.onload=function(){
+//         var jsonArray = JSON.parse(reader.result);
+//         // console.log(JSON.parse(reader.result));
+//         // console.log(jsonArray["name"]);
+//         for (var key in jsonArray){
+//             jsonText = jsonText + key + ": " + jsonArray[key] + "<br>";
+//         }
+//         jsonOutput.innerHTML = jsonText;
+//     }
+// }
